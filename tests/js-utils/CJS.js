afb00fb03f569b725d32701b85cdddc6cb999db2
jedrzejratajczak <jedrzej.ratajczak@htdevelopers.com>
2023-06-23 10:26:18 +0200

    x

---
+++ tests/js-utils/CJS.js
@@ -0,0 +1,3 @@
+const average = require('./dist/index.cjs.js');
+
+console.log(average(5, 10, 50, -45, 6, 7));
---
+++ tests/js-utils/ESM.js
@@ -0,0 +1,3 @@
+import average from './dist/index.es.js';
+
+console.log(average(5, 10, 50, -45, 6, 7));
